cef8b3167500e110ee2593b52cefe2910f1acfe9
mnsekh111 <mnsekh111@gmail.com>
2016-10-21 21:49:41 -0400

    Added bot

--- bot.js
+++ bot.js
@@ -3,21 +3,25 @@
  */
 
 var Botkit = require('botkit');
-
+var settings = require('./settings.json');
 var controller = Botkit.slackbot();
 
+console.log(settings);
 var bot = controller.spawn({
-    token: my_slack_bot_token
+    token: settings.bot_token
 })
 
 bot.startRTM(function(err,bot,payload) {
     if (err) {
         throw new Error('Could not connect to Slack');
     }
-
-    // close the RTM for the sake of it in 5 seconds
-    setTimeout(function() {
-        bot.closeRTM();
-    }, 5000);
 });
 
+controller.hears('hello', 'direct_message', function(bot, message) {
+    console.log(message)
+    bot.replyPublic(message,{
+        text:"hello world"
+    },function(err){
+
+    });
+});
