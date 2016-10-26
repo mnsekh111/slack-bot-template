4254b7c6e1e40a836bcfa5fb1d152763be9be543
mnsekh111 <mnsekh111@gmail.com>
2016-10-25 21:46:32 -0400

    Added api.ai

--- bot.js
+++ bot.js
@@ -5,23 +5,40 @@
 var Botkit = require('botkit');
 var settings = require('./settings.json');
 var controller = Botkit.slackbot();
+var apiai = require("apiai");
+ai = apiai(settings.apiai_uri);
 
 console.log(settings);
 var bot = controller.spawn({
     token: settings.bot_token
 })
 
-bot.startRTM(function(err,bot,payload) {
+bot.startRTM(function (err, bot, payload) {
     if (err) {
         throw new Error('Could not connect to Slack');
     }
 });
 
-controller.hears('hello', 'direct_message', function(bot, message) {
+controller.hears('hello', 'direct_message', function (bot, message) {
     console.log(message)
-    bot.replyPublic(message,{
-        text:"hello world"
-    },function(err){
-
+    var request = ai.textRequest("what is your name")
+    request.on('response', function (response) {
+        console.log(response)
+        bot.reply(message, {
+            attachments: [
+                {
+                    title: response,
+                    callback_id: '123',
+                    attachment_type: 'default'
+                }
+            ]
+        });
     });
+    request.on('response', function (response) {
+        console.log(response)
+    });
+
+    request.end();
+
 });
+
